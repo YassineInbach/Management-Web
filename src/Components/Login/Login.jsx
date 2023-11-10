f375b00b17f27e623cd4cb4c17beea4ad26598a3
YassineInbach <yassine.inbach@gmail.com>
2023-11-10 12:01:10 +0100

    Updtae Login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
   }, [Close]);
   function handleClick(){
    setClose(!Close);
-   return window.location.href = "/Tasker";
+   return window.location.href = "/";
   }
 
   return (
